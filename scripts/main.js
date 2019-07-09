7b6133afc7e7dbf5b88b9650c57eb543bd16afd6
Daniel Collins <mrdanieljcollins@gmail.com>
2019-07-08 20:45:24 -0600

    Updated activity page

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    scrollVal = window.scrollY + this.window.innerHeight;
-    missionY = this.document.getElementById("mission").offsetTop;
-
-    if(scrollVal >= missionY){
-        //console.log(missionY);
-    }
-
-});

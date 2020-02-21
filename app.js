e5acf6f51a44c64377fc92ce8792714f7bf2eba0
Daniel Collins <danielcollins@Daniels-MacBook-Pro.local>
2020-02-20 22:51:41 -0800

    set up to start server correctly

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Constant variables
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//Middleware
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

2d675e090cfc698463e4fc34495997df79ad8141
Daniel Collins <danielcollins@Daniels-MacBook-Pro.local>
2020-04-03 18:17:21 -0600

    fixed issues that broke page

---
+++ scripts/nav-footer-loader.js
@@ -0,0 +1,6 @@
+    // Dynamically loaded components
+    $( document ).ready(function () {
+        $('#comp-nav').load('/pages/components/nav.html');
+        $('#comp-donate').load('/pages/components/donate.html');
+        $('#comp-foot').load('/pages/components/foot.html');
+      });

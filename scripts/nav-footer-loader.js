ccd39ea70e09fe1fd683c3990dbf90778dabcdf3
Daniel Collins <danielcollins@Daniels-MacBook-Pro.local>
2020-12-11 09:53:26 -0700

    Fixed pathing issue

--- scripts/nav-footer-loader.js
+++ scripts/nav-footer-loader.js
@@ -1,6 +1,6 @@
     // Dynamically loaded components
     $( document ).ready(function () {
-        $('#comp-nav').load('components/nav.html');
-        $('#comp-donate').load('components/donate.html');
-        $('#comp-foot').load('components/foot.html');
+        $('#comp-nav').load('/Camp-Salvation/public/pages/components/nav.html');
+        $('#comp-donate').load('/Camp-Salvation/public/pages/components/donate.html');
+        $('#comp-foot').load('/Camp-Salvation/public/pages/components/foot.html');
       });

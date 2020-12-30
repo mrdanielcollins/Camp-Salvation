6e9ffde01d2c17890bbd27f68eca774bd43f05a6
Daniel Collins <danielcollins@Daniels-MacBook-Pro.local>
2020-12-30 14:34:08 -0700

    added database information

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,27 +1,54 @@
+// send email to camp salvation
 function sendEmail(email, subject, content) {
     // Verify data is not empty
-    if (email == '' || subject == '' || content == ''){
+    if (email == '' || subject == '' || content == '') {
         return;
     }
 
     // Validate the email is good
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
         return;
     }
 
 
     // Send email data
-    emailData = { subject : subject , email : email , content : content}
+    emailData = { subject: subject, email: email, content: content }
     $.ajax({
         url: "https://obscure-plateau-25160.herokuapp.com/sendEmail",
         type: "POST",
         crossDomain: true,
         data: emailData,
-        dataType: "json"            
+        dataType: "json"
     });
+} 
+
+// add emil to emaillist
+function addEmail(email) {
+
+    // Verify data is not empty
+    if (email == '') {
+        return false;
+    }
+
+    // Validate the email is good
+    if (!validateEmail(email)) {
+        return false;
+    }
+
+    // Send add email
+    emailData = { email: email }
+    $.ajax({
+        url: "https://obscure-plateau-25160.herokuapp.com/addEmail",
+        type: "POST",
+        crossDomain: true,
+        data: emailData,
+        dataType: "json"
+    });
+
+    return;
 }
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-  }
+}

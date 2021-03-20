c8b9cec2a57d123db413b540d6ef47d02dc0d361
Daniel Collins <danielcollins@Daniels-MacBook-Pro.local>
2021-03-20 09:10:50 -0600

    Updated to work on website

--- public/scripts/nav-footer-loader.js
+++ public/scripts/nav-footer-loader.js
@@ -1,6 +1,6 @@
     // Dynamically loaded components
     $( document ).ready(function () {
-        $('#comp-nav').load('/Camp-Salvation/public/pages/components/nav.html');
-        $('#comp-donate').load('/Camp-Salvation/public/pages/components/donate.html');
-        $('#comp-foot').load('/Camp-Salvation/public/pages/components/foot.html');
+        $('#comp-nav').load('/pages/components/nav.html');
+        $('#comp-donate').load('/pages/components/donate.html');
+        $('#comp-foot').load('/pages/components/foot.html');
       });
